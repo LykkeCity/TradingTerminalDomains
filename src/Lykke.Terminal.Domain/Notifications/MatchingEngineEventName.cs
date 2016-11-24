0b666ae7c2fc506334125f775fbdeab90ab60e0c
Julia Tolipova <tuxina@yandex.ru>
2016-11-24 15:42:51 +0500

    renamed class

--- src/Lykke.Terminal.Domain/Notifications/MatchingEngineEventName.cs
+++ src/Lykke.Terminal.Domain/Notifications/MatchingEngineEventName.cs
@@ -2,11 +2,12 @@
 
 namespace Lykke.Terminal.Domain.Notifications
 {
-    public class MatchingEngineTopics
+    public class MatchingEngineEventName
     {
         public const string AccountUpdated = "AccountUpdated";
         public const string AssetPairPriceUpdated = "AssetPairPriceUpdated";
         public const string ActiveOrdersUpdated = "ActiveOrdersUpdated";
+        public const string OrderBookUpdated = "lykke.orderbook";
 
         public static IEnumerable<string> AllTopics => new List<string>
         {
