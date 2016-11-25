1bd39fddb7c8f43da7adf86474d84fa32119c192
Julia Tolipova <tuxina@yandex.ru>
2016-11-25 16:41:00 +0500

    added asset pair dictionary service

---
+++ src/Lykke.Terminal.Domain/Dictionaries/IAssetPairDictionaryService.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lykke.Terminal.Domain.Assets;
+
+namespace Lykke.Terminal.Domain.Dictionaries
+{
+    public interface IAssetPairDictionaryService
+    {
+        Task<IEnumerable<IAssetPair>> GetAllAsync();
+        Task<IAssetPair> GetByBaseAndQuotingIds(string baseAssetId, string quotingAssetId);
+        Task<IAssetPair> GetByIdAsync(string id);
+    }
+}
