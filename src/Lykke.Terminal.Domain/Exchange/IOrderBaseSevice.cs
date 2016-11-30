6b591712c7cce43381f42752e229197fe0e7a0d5
Julia Tolipova <tuxina@yandex.ru>
2016-11-30 11:23:49 +0500

    introduced order base service

---
+++ src/Lykke.Terminal.Domain/Exchange/IOrderBaseSevice.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Lykke.Terminal.Domain.Exchange
+{
+    public interface IOrderBaseSevice
+    {
+        Task MoveOrderToDoneAsync(IOrderBase order, OrderStatus status = OrderStatus.Done,
+            double profitLoss = double.NaN);
+    }
+}
