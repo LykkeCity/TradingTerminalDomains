bce4422013d30b8204f7c17f80769ac4eb340bbe
Julia Tolipova <tuxina@yandex.ru>
2016-12-05 16:04:08 +0500

    introduced pending order service interface

---
+++ src/Lykke.Terminal.Domain/Exchange/IPendingOrderService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Lykke.Terminal.Domain.Exchange
+{
+    public interface IPendingOrderService
+    {
+        Task MoveOrderToActiveAsync(IOrderBase pendingOrder, IAssetPairQuote assetPairQuote);
+    }
+}
