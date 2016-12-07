888411b7bf53e2487a062bbd418a85aa775bbf50
Julia Tolipova <tuxina@yandex.ru>
2016-12-07 18:01:17 +0500

    extended asset pair model

--- src/Lykke.Terminal.Domain/Assets/IAssetPairRepository.cs
+++ src/Lykke.Terminal.Domain/Assets/IAssetPairRepository.cs
@@ -12,6 +12,8 @@ namespace Lykke.Terminal.Domain.Assets
         string BaseAssetId { get; }
         string QuotingAssetId { get; }
         int InvertedAccuracy { get; }
+        double SwapShort { get; }
+        double SwapLong { get; }
     }
 
     public class AssetPair : IAssetPair
@@ -22,6 +24,8 @@ namespace Lykke.Terminal.Domain.Assets
         public string BaseAssetId { get; set; }
         public string QuotingAssetId { get; set; }
         public int InvertedAccuracy { get; set; }
+        public double SwapShort { get; set; }
+        public double SwapLong { get; set; }
     }
 
     public interface IAssetPairRepository : IRepository<IAssetPair>
