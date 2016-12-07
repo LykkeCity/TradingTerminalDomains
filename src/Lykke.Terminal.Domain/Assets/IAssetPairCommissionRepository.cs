632767f4aa23472d8a0a0994b559fadd67a7783e
Julia Tolipova <tuxina@yandex.ru>
2016-12-07 18:01:40 +0500

    introduced asset pair commission

---
+++ src/Lykke.Terminal.Domain/Assets/IAssetPairCommissionRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace Lykke.Terminal.Domain.Assets
+{
+    public interface IAssetPairCommission
+    {
+        string AssetPairId { get; }
+        double OpenCommission { get; }
+        double CloseCommission { get; }
+    }
+
+    public class AssetPairCommission : IAssetPairCommission
+    {
+        public string AssetPairId { get; set; }
+        public double OpenCommission { get; set; }
+        public double CloseCommission { get; set; }
+    }
+}
---
+++ src/Lykke.Terminal.Domain/Dictionaries/IAssetPairCommissionDictionaryService.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Lykke.Terminal.Domain.Assets;
+
+namespace Lykke.Terminal.Domain.Dictionaries
+{
+    public interface IAssetPairCommissionDictionaryService
+    {
+        Task<IEnumerable<IAssetPairCommission>> GetAllAsync();
+        Task<IAssetPairCommission> GetByAssetPairId(string assetPairId);
+    }
+}
