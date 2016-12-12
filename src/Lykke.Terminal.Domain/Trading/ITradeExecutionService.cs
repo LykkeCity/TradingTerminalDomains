3b046540f0fe071c2f7f50b02b72296e3f1a2c40
Julia Tolipova <tuxina@yandex.ru>
2016-12-12 15:42:15 +0300

    introduced trade execution service interface

---
+++ src/Lykke.Terminal.Domain/Trading/ITradeExecutionService.cs
@@ -0,0 +1,9 @@
+﻿using System.Threading.Tasks;
+
+namespace Lykke.Terminal.Domain.Trading
+{
+    public interface ITradeExecutionService
+    {
+        Task<bool> CheckTradeRequestAsync(string orderId, string asset, double volume, double price);
+    }
+}
