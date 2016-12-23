591d95e9a19bd649d3f235e5a7eb24778f0e2d02
Julia Tolipova <julia.tolipova@gmail.com>
2016-12-23 13:58:35 +0300

    send top order book dto on market profile request

--- src/Lykke.Terminal.Domain/ApplicationServices/Exchange/IMarketProfileAppService.cs
+++ src/Lykke.Terminal.Domain/ApplicationServices/Exchange/IMarketProfileAppService.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using Lykke.Terminal.Domain.Exchange;
 
 namespace Lykke.Terminal.Domain.ApplicationServices.Exchange
 {
     public interface IMarketProfileAppService : IAppService
     {
-        Task<IEnumerable<AssetPairQuote>> GetMarketProfileAsync();
+        Task<IEnumerable<TopOrderBookDto>> GetMarketProfileAsync();
     }
 }
---
+++ src/Lykke.Terminal.Domain/ApplicationServices/Exchange/TopOrderBookDto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Lykke.Terminal.Domain.ApplicationServices.Exchange
+{
+    public class TopOrderBookDto
+    {
+        public string InstrumentId { get; set; }
+
+        public DateTime Timestamp { get; set; }
+
+        public decimal Bid { get; set; }
+
+        public decimal Ask { get; set; }
+    }
+}
