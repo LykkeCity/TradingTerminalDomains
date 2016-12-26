e70fd026762e31efe541713b1c19e0c0717cfbc3
Julia Tolipova <julia.tolipova@gmail.com>
2016-12-26 10:37:52 +0300

    renamed service

---
+++ src/Lykke.Terminal.Domain/Exchange/IProductQuoteService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+
+namespace Lykke.Terminal.Domain.Exchange
+{
+    public interface IProductQuoteService
+    {
+        Task UpdateOrdersOnQuoteChangeAsync(IProductQuote productQuote);
+        Task<ProductQuote> GetProductQuoteAsync(string instrumentId);
+    }
+}
